46b1029f19761a200871f78f6f09cc4424fd68ad
Rubgrsch <rubgrsch@gmail.com>
2017-09-28 15:28:13 +0800

    Add files

---
+++ RgsCT.lua
@@ -0,0 +1,155 @@
+local format, GetSpellTexture = format, GetSpellTexture
+
+local myGUID
+
+-- Change CVars
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_LOGIN")
+f:SetScript("OnEvent", function()
+	SetCVar("floatingCombatTextCombatDamage", 0)
+	SetCVar("floatingCombatTextCombatHealing", 0)
+	SetCVar("enableFloatingCombatText", 0)
+
+	myGUID = UnitGUID("player")
+end)
+
+local function NumUnitFormat(value)
+	if value > 1e8 then
+		return format("%.1fY",value/1e8)
+	elseif value > 1e4 then
+		return format("%.1fW",value/1e4)
+	else
+		return format("%d",value)
+	end
+end
+
+
+local dmgcolor = {
+	[1] = "ffff00",
+	[2] = "ffe57f",
+	[4] = "ff7f00",
+	[8] = "4cff4c",
+	[16] = "7fffff",
+	[32] = "7f7fff",
+	[64] = "ff7fff",
+	[9] = "a5ff26",
+	[18] = "bff2bf",
+	[36] = "bf7f7f",
+	[5] = "ffbf00",
+	[10] = "bff2bf",
+	[20] = "bfbf7f",
+	[40] = "66bfa5",
+	[80] = "bfbfff",
+	[127] = "c1c48c",
+	[126] = "b7baa3",
+	[3] = "fff23f",
+	[6] = "ffb23f",
+	[12] = "a5bf26",
+	[24] = "66ffa5",
+	[48] = "7fbfff",
+	[65] = "ffbf7f",
+	[124] = "a8b2a8",
+	[66] = "ffb2bf",
+	[96] = "bf7fff",
+	[72] = "a5bfa5",
+	[68] = "ff7f7f",
+	[28] = "99d670",
+	[34] = "bfb2bf",
+	[33] = "bfbf7f",
+	[17] = "bfff7f",
+}
+
+local EventList = {
+	SWING_DAMAGE = 1,
+	SPELL_DAMAGE = 2,
+	RANGE_DAMAGE = 2,
+	SPELL_PERIODIC_DAMAGE = 2,
+	SWING_MISSED = 3,
+	SPELL_MISSED = 4,
+	RANGE_MISSED = 4,
+	SPELL_HEAL = 5,
+	SPELL_PERIODIC_HEAL = 5,
+	ENVIRONMENTAL_DAMAGE = 6,
+}
+
+local function CreateCTFrame(name, ...)
+	local frame = CreateFrame("ScrollingMessageFrame", name, UIParent)
+
+	frame:SetSpacing(3)
+	frame:SetMaxLines(20)
+	frame:SetSize(120,150)
+	frame:SetFadeDuration(0.2)
+	frame:SetTimeVisible(3)
+	frame:SetFont(STANDARD_TEXT_FONT, 14, "OUTLINE")
+	frame:SetPoint(...)
+
+	return frame
+end
+local OutFrame = CreateCTFrame("RgsCTOut","LEFT", UIParent, "CENTER",200,0)
+local InFrame = CreateCTFrame("RgsCTIn","RIGHT", UIParent, "CENTER",-200,0)
+
+
+local function DamageString(isIn,spellID,amount,school,isCritical)
+	(isIn and InFrame or OutFrame):AddMessage(format(isCritical and "|T%s:0:0:0:-5|t|cff%s*%s%s*|r" or "|T%s:0:0:0:-5|t|cff%s%s%s|r",GetSpellTexture(spellID),dmgcolor[school],isIn and "-" or "",NumUnitFormat(amount)))
+end
+
+local function MissString(isIn,spellID,missType)
+	(isIn and InFrame or OutFrame):AddMessage(format("|T%s:0:0:0:-5|t%s",GetSpellTexture(spellID),_G[missType]))
+end
+
+local function HealString(isIn,spellID,amount,spellSchool,isCritical)
+	(isIn and InFrame or OutFrame):AddMessage(format(isCritical and "|T%s:0:0:0:-5|t|cff%s*+%s*|r" or "|T%s:0:0:0:-5|t|cff%s+%s|r",GetSpellTexture(spellID),dmgcolor[spellSchool],NumUnitFormat(amount)))
+end
+
+local function EnvironmantalString(environmentalType,amount,spellSchool)
+	InFrame:AddMessage(format("|cff%s%s-%s|r",dmgcolor[spellSchool],_G["ACTION_ENVIRONMENTAL_DAMAGE_"..(environmentalType:upper())],NumUnitFormat(amount)))
+end
+
+local fromMe, toMe
+local spellId, spellSchool
+local amount, overkill, school, critical
+local missType, overhealing, environmentalType
+local function parseCT(_,_,_, event, _, sourceGUID, _, _, _, destGUID, _, _, _, ...)
+	fromMe = sourceGUID == myGUID
+	toMe = destGUID == myGUID
+	if EventList[event] == 1 then
+		amount, overkill, school, _, _, _, critical = ...
+		if overkill > 0 then amount = amount - overkill end
+		if amount > 0 then
+			if fromMe then DamageString(false,6603,amount,school,critical) end
+			if toMe then DamageString(true,6603,amount,school,critical) end
+		end
+	elseif EventList[event] == 2 then
+		spellId, _, _, amount, overkill, school, _, _, _, critical = ...
+		if overkill > 0 then amount = amount - overkill end
+		if amount > 0 then
+			if fromMe then DamageString(false,spellId,amount,school,critical) end
+			if toMe then DamageString(true,spellId,amount,school,critical) end
+		end
+	elseif EventList[event] == 3 then
+		missType = ...
+		if fromMe then MissString(false,6603,missType) end
+		if toMe then MissString(true,6603,missType) end
+	elseif EventList[event] == 4 then
+		spellId, _, _, missType = ...
+		if fromMe then MissString(false,spellId,missType) end
+		if toMe then MissString(true,spellId,missType) end
+	elseif EventList[event] == 5 then -- Healing accept
+		spellId, _, spellSchool, amount, overhealing, _, critical = ...
+		if overhealing > 0 then amount = amount - overhealing end
+		if amount > 0 then
+			if toMe then HealString(true,spellId,amount,spellSchool,critical)
+			elseif fromMe then HealString(false,spellId,amount,spellSchool,critical) end
+		end
+	elseif EventList[event] == 6 then
+		environmentalType, amount, overkill, school = ...
+		if overkill > 0 then amount = amount - overkill end
+		if amount > 0 then
+			if toMe then EnvironmantalString(environmentalType,amount,school) end
+		end
+	end
+end
+
+local eventFrame = CreateFrame("Frame")
+eventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+eventFrame:SetScript("OnEvent", parseCT)
