72c747dcce004be4e15ff0d7f5e5fcc91280e8e3
EK <eke00372@gmail.com>
2018-02-05 21:51:07 +0800

    Update zhTW.lua

--- locales/zhTW.lua
+++ locales/zhTW.lua
@@ -7,7 +7,7 @@ if GetLocale() ~= "zhTW" then return end
 
 L["In"] = "承受"
 L["Out"] = "輸出"
-L["Info"] = "信息" --<<
+L["Info"] = "訊息"
 
 L["fontSize"] = "字型大小"
 L["mover"] = "調整位置"
@@ -18,12 +18,12 @@ L["merge"] = "合併顯示"
 L["mergeTooltip"] = "嘗試合併顯示多目標傷害與治療。"
 L["showMyPet"] = "顯示玩家寵物"
 L["showMyPetTooltip"] = "顯示玩家的寵物造成的傷害與治療。"
-L["periodicTooltip"] = "显示周期性伤害/治疗" --<<
-L["showInfo"] = "战斗信息" --<<
-L["showInfoTooltip"] = "显示部分与你有关的战斗信息，比如进入战斗/成功打断。" --<<
-L["InterruptedSpell"] = "已打断%s的[%s]" --<<
-L["Dispeled"] = "已驱散%s的[%s]" --<<
-L["Stole"] = "已偷取%s的[%s]" --<<
+L["periodicTooltip"] = "顯示周期性傷害與治療"
+L["showInfo"] = "戰鬥訊息"
+L["showInfoTooltip"] = "顯示部分與你有關的戰鬥訊息，比如進入戰鬥、成功打斷。"
+L["InterruptedSpell"] = "已打斷%s的[%s]"
+L["Dispeled"] = "已驅散%s的[%s]"
+L["Stole"] = "已偷取%s的[%s]"
 L["NumUnitFormat"] = function(value)
 	if value > 1e8 then
 		return format("%.1f億",value/1e8)
